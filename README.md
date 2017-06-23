914ef407216ecb7ee6a8a797a6ff4b640b298d54
Rajdeepc <rajrock38@gmail.com>
2017-06-23 10:53:50 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+Steps to Install:
+
+Go to Google developer console.
+
+Register your Youtube Data API v3.
+
+Once you get your API key place your api key in the project.
+
+
+Run npm install youtube-search-api to install the dependency of the search api
+
+Run npm install Lodash to install Lodash
+
+Run npm start to spin up the lite server 
---
+++ src/index.js
@@ -0,0 +1,57 @@
+
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+
+
+import _ from 'lodash';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+import YTSearch from 'youtube-api-search';
+const API_KEY = 'AIzaSyAhbbUYwWnSTXHltgZhsI_myy2m-MDW0_w';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+        this.videoSearch("surfboards");
+
+    }
+
+
+videoSearch(term){
+    YTSearch({ key: API_KEY, term: term }, videos => {
+        this.setState({
+            videos: videos,
+            selectedVideo: videos[0]
+        });
+    });
+}
+
+render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
+
+
+    return (
+        <div>
+            <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+            <VideoDetail video={this.state.selectedVideo} />
+            <VideoList
+                onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+                videos={this.state.videos} />
+        </div>
+    );
+}
+}
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
